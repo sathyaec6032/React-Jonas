b3858f292af04ceae98cfa33f23fbf1e9b315b98
sathyaec6032 <sathyaec6032@gmail.com>
2023-08-24 15:35:25 +0530

    section-17 added

---
+++ Section-17/worldwise/src/components/Map.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./Map.module.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+export default function Map() {
+  const navigate = useNavigate()
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams)
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer} onClick={() => {navigate('form')}}>
+      <h1>Map</h1>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        Change Position
+      </button>
+    </div>
+  );
+}
