0ee78798fc599932145f1e33337eb8b96783409c
sathyaec6032 <sathyaec6032@gmail.com>
2023-09-13 17:52:10 +0530

    Section 23 half way crossed

--- Section-23/fast-react-pizza/src/ui/Header.jsx
+++ Section-23/fast-react-pizza/src/ui/Header.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="flex items-center justify-between border-b border-stone-500 bg-pizza px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Sathya</p>
+      <Username />
     </header>
   );
 }
--- Section-23/fast-react-pizza/src/ui/Loader.jsx
+++ Section-23/fast-react-pizza/src/ui/Loader.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function Loader() {
   return (
-    <div className='loader'></div>
-  )
+    <div className='absolute bg-slate-200/20 inset-0 backdrop-blur-sm flex items-center justify-center'>
+      <div className="loader"></div>
+    </div>
+  );
 }
